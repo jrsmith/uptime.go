80ec0733f8a3427f9046f4630baae6ceccd8201c
JR Smith <jr@thisisgrow.com>
2013-12-03 13:49:04 -0500

    Updated script to use a config file Added domain to email alert subject

--- uptime.go
+++ uptime.go
@@ -1,82 +1,108 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/smtp"
+	"os"
+	"runtime"
 	"sync"
 	"time"
 )
 
 var wg sync.WaitGroup
+var config Config
 
 const delay = 1e9 * 100
 
+type SMTPConfig struct {
+	Username       string
+	Password       string
+	Host           string
+	OutgoingServer string `json:"outgoing_server"`
+	From           string
+}
+
+type Config struct {
+	ToEmail    []string `json:"to_email"`
+	DomainList []string `json:"domain_list"`
+	SMTP       SMTPConfig
+}
+
 func main() {
 
-	var domain_list = []string{
-		"http://google.com",
-		"http://facebook.com",
-		"http://reddit.com",
+	runtime.GOMAXPROCS(4)
+
+	configFile, err := ioutil.ReadFile("./config.json")
+
+	if err != nil {
+		fmt.Printf("Failed to read config file: %v\n", err)
+		os.Exit(1)
 	}
 
+	json.Unmarshal(configFile, &config)
+
 	for {
-		go ping(domain_list)
+		go ping(config.DomainList)
 		time.Sleep(delay)
 	}
 
 }
 
-func ping(domain_list []string) {
-	for i := range domain_list {
+func ping(domainList []string) {
+	for i := range domainList {
 
-		fmt.Println("Pinging", domain_list[i])
+		fmt.Println("Pinging", domainList[i])
 
 		wg.Add(1)
 
 		go func(domain string) {
 
-			res, err := http.Head(domain)
+			url := fmt.Sprintf("http://%s", domain)
+
+			res, err := http.Head(url)
 
 			if err == nil {
 				if res.StatusCode == 200 {
 					fmt.Println("Site is up")
 				} else {
 					fmt.Println("Site is down:", res.Status)
-					go alert(res.Status)
+					go alert(domain, res.Status)
 				}
 			} else {
 				fmt.Println("Site didn't respond:", err)
-				go alert(err.Error())
+				go alert(domain, err.Error())
 			}
 
 			wg.Done()
 
-		}(domain_list[i])
+		}(domainList[i])
 
 		wg.Wait()
 
 	}
 }
 
-func alert(error_message string) {
+func alert(domain string, error_message string) {
 
 	fmt.Println("Sending mail")
 
-	subject := "Subject: Test\r\n\r\n"
+	subject := fmt.Sprintf("Subject: %s is down\r\n\r\n", domain)
 
 	auth := smtp.PlainAuth(
 		"",
-		"noreply@example.com",
-		"password",
-		"smtp.example.com",
+		config.SMTP.Username,
+		config.SMTP.Password,
+		config.SMTP.Host,
 	)
 
 	err := smtp.SendMail(
-		"smtp.example.com:587",
+		config.SMTP.OutgoingServer,
 		auth,
-		"noreply@example.com",
-		[]string{"recipient@example.com"},
+		config.SMTP.From,
+		config.ToEmail,
 		[]byte(subject+error_message),
 	)
 
